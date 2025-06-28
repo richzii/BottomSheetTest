6f24481b197fd6b98b4c311e052bbab322a68ee7
Rihards Ziverts <rihards.ziverts@gmail.com>
2025-06-28 23:08:12 +0300

    Project created.

---
+++ app/src/main/java/com/bottomsheettest/app/ui/theme/Theme.kt
@@ -0,0 +1,38 @@
+package com.bottomsheettest.app.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer
+)
+private val DarkColorScheme = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer
+)
+
+@Composable
+fun BottomSheetTestTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme =
+        if (!darkTheme) {
+            LightColorScheme
+        } else {
+            DarkColorScheme
+        }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        shapes = Shapes,
+        typography = Typography,
+        content = content
+    )
+}
